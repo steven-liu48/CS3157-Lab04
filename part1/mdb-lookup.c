5eced95c6c1d66e158d5f3bfd0ec6e56b3d31b33
Steven Liu <xl2948@columbia.edu>
2020-03-09 22:49:45 -0400

    Finished most part

--- part1/mdb-lookup.c
+++ part1/mdb-lookup.c
@@ -2,6 +2,7 @@
 #include "mylist.h"
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
 
 struct Node *appendNode(struct List *list, struct MdbRec *data){
 	struct Node *pointer = list -> head;
@@ -12,6 +13,20 @@ struct Node *appendNode(struct List *list, struct MdbRec *data){
 		pointer = pointer -> next;
 	}
 	return addAfter(list, pointer, data);
+}
+
+void findData(char *string, struct List *list){
+	struct Node *pointer = list -> head;
+	int counter = 1;
+	while (pointer != NULL){
+		//printf("%d \n", counter++);
+		struct MdbRec *data = pointer -> data;
+		if (strstr(data -> name, string) ||strstr(data -> msg, string)){
+			printf("   %d: {%s} said {%s} \n", counter, data -> name, data -> msg);
+		}
+		counter++;
+		pointer = pointer -> next;
+	}
 }
 
 void freeData(){
@@ -35,8 +50,6 @@ int main(int argc, char **argv)
 	struct List list;
 	initList(&list);
     
-	
-
 	//Read data into the list
     while (1){
 		struct MdbRec *buffer = malloc(40);
@@ -49,6 +62,16 @@ int main(int argc, char **argv)
 		}
     }
 	
+	//Take in user input and look for it
+	printf("lookup: ");
+	char input[1000];
+	while (scanf("%s", input) != EOF){
+		//printf("%s \n", input);
+		findData(input, &list);
+		printf("lookup: ");
+	}
+	
+	//Free everything
 	
 	//Close file and end
 	fclose(fp);
