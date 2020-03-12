15c085c2fb40f3d599d897ccd7241685223bf83d
Steven Liu <xl2948@columbia.edu>
2020-03-11 22:56:13 -0400

    All major functions should work properly now.

--- part1/mdb-lookup.c
+++ part1/mdb-lookup.c
@@ -16,6 +16,27 @@ struct Node *appendNode(struct List *list, struct MdbRec *data){
 	return addAfter(list, pointer, data);
 }
 
+void setEnd(struct List *list){
+	struct Node *pointer = list -> head;
+	struct MdbRec *dataPointer;
+	while(pointer -> next != NULL){
+		dataPointer = pointer -> data;
+		char *n = dataPointer -> name;
+		int i = 0;
+		while(isprint(n[i])){
+			i++;
+		}
+		n[i] = '\0';
+		i = 0;
+		n = dataPointer -> msg;
+		while(isprint(n[i])){
+            i++;
+        }
+		n[i] = '\0';
+        pointer = pointer -> next;
+    }
+}
+
 void findData(char *string, struct List *list){
 	struct Node *pointer = list -> head;
 	int counter = 1;
@@ -23,7 +44,7 @@ void findData(char *string, struct List *list){
 		struct MdbRec *data = pointer -> data;
 		//printf("%d%d ", strstr(data -> name, string) != NULL, strstr(data -> msg, string) != NULL);
 		//printf("%s %s %s \n", data->name, data->msg, string);
-		if (strstr(data -> name, string) ||strstr(data -> msg, string)){
+		if (strstr(data -> name, string) != NULL||strstr(data -> msg, string) != NULL){
 			if (counter < 10) printf("   ");
 			if (counter < 100 && counter >=10) printf("  ");
 			if (counter >= 100) printf(" ");
@@ -65,32 +86,51 @@ int main(int argc, char **argv)
     while (1){
 		struct MdbRec *buffer = malloc(40);
 		if (fread(buffer, 40, 1, fp) == 1){
-			printf("%s ", buffer -> name);
-	        printf("%s \n", buffer -> msg);
+			//printf("%s ", buffer -> name);
+	        //printf("%s \n", buffer -> msg);
 			appendNode(&list, buffer);
 		}else{
+			free(buffer);
 			break;
 		}
+		//free(buffer);
     }
 	
+	//Set the end of every string '\n'
+	setEnd(&list);
+	
+	//Print out everything for test
+	/*
+	struct Node *pointer = (&list) -> head;
+	struct MdbRec *buffer;
+    while(pointer -> next != NULL){
+		buffer = pointer -> data;
+		printf("%s ", buffer -> name);
+        printf("%s \n", buffer -> msg);
+        pointer = pointer -> next;
+    }
+	*/
+
 	//Take in user input and look for it
 	printf("lookup: ");
 	char input[1000];
 	while (fgets (input, 1000, stdin) != NULL){
-		char *proc = malloc(6);
+		char *proc = calloc(6, 1);
 		strncpy(proc, input, 5);
-		for(int i=0; i<5; i++){
+		for(int i=0; i<6; i++){
 			if(!isprint(proc[i])){
 				proc[i] = '\0';
 			}
 		}
+		//printf("%s", proc);
+		//printf("%d", strlen(proc));
 		findData(proc, &list);
 		free(proc);
 		printf("\nlookup: ");
 	}
 	
 	//Free everything
-	freeData(list);
+	freeData(&list);
 	while ((popFront(&list)) != NULL) {
         ;
     }
